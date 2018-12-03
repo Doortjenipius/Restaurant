7292f489139ad1b9acf33084418c192a40c4d140
doortje <doortje@doortjes-MacBook-Pro.local>
2018-12-03 11:30:45 +0100

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by doortje on 03/12/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
